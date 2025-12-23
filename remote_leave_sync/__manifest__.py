596d074707ce4d68648e7f06c576229e726dfe75
mohamed-wanaag <backend@wanaag.co.ke>
2025-12-23 16:51:52 +0300

    code files upload

---
+++ remote_leave_sync/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': 'Remote Leave Management Sync',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'author': 'Wanaag Solutions',
+    'website': 'https://wanaag.odoo.com/',
+    'license': 'OPL-1',
+    'price': '249.99',
+    'currency': 'USD',
+    'support': 'backend@wanaag.co.ke',
+    'summary': 'Synchronize employee leave requests with remote Odoo database using OdooRPC',
+    'description': """
+        Sync employee leave/time-off requests between local and remote Odoo instances.
+        - Uses OdooRPC library for cleaner, more Pythonic remote calls
+        - Automatic sync when leave is requested, approved, or refused
+        - Bidirectional sync support
+        - Multiple sync configurations
+    """,
+    'depends': ['hr_holidays'],
+    'external_dependencies': {
+        'python': ['odoorpc'],  # Requires: pip install OdooRPC
+    },
+    'data': [
+        'security/ir.model.access.csv',
+        'views/leave_sync_views.xml',
+        'views/menu_views.xml',
+    ],
+    'images': ['images/main_screenshot.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
